05324a0cc29c60809f610bb278e752a8eea7296c
khumora77 <xumora427@gmail.com>
2025-08-29 18:01:01 +0500

    df

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const { t } = useTranslation();
@@ -10,20 +11,19 @@ export default function Footer() {
           <h2 className="text-2xl font-bold uppercase text-red-500 mb-2">
             {t("footer.orderTitle")}
           </h2>
-          <p className="text-lg font-semibold">📞 90 303 59 35</p>
+          <Link to={"/menu"}>
+            <p className="text-lg font-semibold">📞 90 303 59 35</p>
+          </Link>
         </div>
-
         <div className="mt-2 text-gray-300 text-center md:text-left">
           <p>{t("footer.hoursLabel")}</p>
           <p>🕘 09:30 - 23:00</p>
           <p>🕙 10:30 - 22:30</p>
         </div>
-
         <div className="text-center md:text-right">
           <h3 className="text-5xl font-bold mb-1">Asr-Somsa</h3>
         </div>
       </div>
-
       <div className="mt-8 text-center text-sm text-gray-500">
         © {new Date().getFullYear()} Olot Somsa. {t("footer.rights")}
       </div>
--- src/pages/Bo'g'lanish.tsx
+++ src/pages/Bo'g'lanish.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Contact = () => {
   return (
     <section className="py-16 px-4 bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
